3fde7c4139009d65df27dc0884f1b81f68486de1
Goldobin_AV <geminix@bk.ru>
2020-03-11 16:28:13 +0700

    таски и точки

---
+++ src/points.js
@@ -0,0 +1,12 @@
+let Points;
+export default Points [
+  { idpoint: 1, name: 'точка 1' },
+  { idpoint: 2, name: 'точка 2' },
+  { idpoint: 3, name: 'точка 3' },
+  { idpoint: 4, name: 'точка 4' },
+  { idpoint: 5, name: 'точка 5' },
+  { idpoint: 6, name: 'точка 6' },
+  { idpoint: 7, name: 'точка 7' },
+  { idpoint: 8, name: 'точка 8' },
+  { idpoint: 9, name: 'точка 9' }
+];
--- src/store/task.js
+++ src/store/task.js
@@ -5,7 +5,11 @@ Vue.use(Vuex);
 
 export default {
   state: {
-    list: [1, 2, 3, 3, 4, 6],
+    list: [
+      { taskid: 1, name: 'Задание 1', points: [1, 2, 3] },
+      { taskid: 2, name: 'Задание 2', points: [3, 4, 5] },
+      { taskid: 3, name: 'Задание 3', points: [5, 6, 7, 8, 9, 10] },
+    ],
   },
   mutations: {
   },
